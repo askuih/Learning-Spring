d8eaa6cb5192e04ac06b5b2b6c16c7b371b298eb
Adil <35602213+askuih@users.noreply.github.com>
2023-02-24 18:35:05 +0600

    Create AnimalDto.java

---
+++ entities/dto/AnimalDto.java
@@ -0,0 +1,12 @@
+package sapsan.homework.entities.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnimalDto {
+    private String name;
+    private String species;
+    private int age;
+}
