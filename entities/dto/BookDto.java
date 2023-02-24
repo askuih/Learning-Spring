ab964fba2edabf1c09da6dca9f8c90b4d657f4a5
Adil <35602213+askuih@users.noreply.github.com>
2023-02-24 18:35:22 +0600

    Add files via upload

---
+++ entities/dto/BookDto.java
@@ -0,0 +1,12 @@
+package sapsan.homework.entities.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookDto {
+    private String title;
+    private String author;
+    private int publishYear;
+}
---
+++ entities/dto/BoxerDto.java
@@ -0,0 +1,12 @@
+package sapsan.homework.entities.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BoxerDto {
+    private String name;
+    private int age;
+    private String alias;
+}
